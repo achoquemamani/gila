12c7c3fd74307951e24d60e1dd6c4d373ff98acc
Ariel Choque Mamani <ariel.choquemamani@balloon-group.com>
2023-07-17 00:25:24 -0300

    fix: delete node modules

--- backend/dist/users/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from './users.service';
-import { User } from './user.model';
-export declare class UsersController {
-    private userService;
-    constructor(userService: UsersService);
-    getUser(): Promise<User[]>;
-    createUser(user: User): void;
-    remove(id: string): void;
-}
